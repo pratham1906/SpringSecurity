9359f06091126417a7aea0fa5c898f790a59cf60
Pratham Meghani <premmeghani16@gmail.com>
2022-07-03 19:21:37 +0530

    ControllersDone

--- src/main/java/com/security/Service/UserService.java
+++ src/main/java/com/security/Service/UserService.java
@@ -1,5 +1,38 @@
 package com.security.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.security.entities.User;
+
+@Service
 public class UserService {
 
+	
+	List<User> list=new ArrayList<>();
+
+	public UserService() {
+		list.add(new User("abc","abc","abc@gmail.com"));
+		list.add(new User("xyz","xyz","xyz@gmail.com"));
+		list.add(new User("lca","lca","lca@gmail.com"));
+		list.add(new User("apa","apa","apa@gmail.com"));
+	}
+	
+	//get All Users
+	public List<User> getAllUsers(){
+		return this.list;
+	}
+	
+	//get single user
+	public User getUser(String username) {
+		return this.list.stream().filter((user)->user.getUsername().equals(username)).findAny().orElse(null);
+	}
+	
+	//add User
+	public User addUser(User user) {
+		this.list.add(user);
+		return user;
+	}
 }
--- src/main/java/com/security/controllers/UserController.java
+++ src/main/java/com/security/controllers/UserController.java
@@ -1,5 +1,40 @@
 package com.security.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.Service.UserService;
+import com.security.entities.User;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
+	//all Users
+	@GetMapping("/getAllUsers")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+	
+	@GetMapping("/{username}")
+	public User getUser(@PathVariable String username) {
+		return this.userService.getUser(username);
+	}
+	
+	@PostMapping("/addUser")
+	public User addUser(@RequestBody User user) {
+		return this.userService.addUser(user);
+	}
+	
 }
